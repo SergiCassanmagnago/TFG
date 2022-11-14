5363dfc63be45f3aac613fd58ac9b11da733f960
Sergi Cassanmagnago <sergi.cassanmagnago@gmail.com>
2022-11-14 10:16:20 +0100

    changed structure of dp-wcc

--- DP-WCC/dpwcc.go
+++ DP-WCC/dpwcc.go
@@ -13,11 +13,6 @@ type edge struct {
 	x, y int
 }
 
-type request struct {
-	op string
-	e  edge
-}
-
 func check(e error) {
 	if e != nil {
 		log.Panic(e)
@@ -48,30 +43,26 @@ func union(cc map[int]bool, ccin map[int]bool) map[int]bool {
 }
 
 // Source stage
-func source(istream string, ine chan<- request, inv chan<- map[int]bool) {
+func source(istream string, ine chan<- edge, inv chan<- map[int]bool) {
 	file, err := os.Open(istream + ".requests")
 	check(err)
 	defer file.Close()
 
-	var r request
+	var e edge
 	for {
-		_, err = fmt.Fscanf(file, "%s", &r.op)
+		_, err = fmt.Fscanf(file, "%d%d\n", &e.x, &e.y)
 		if err == io.EOF {
 			break
 		}
 		check(err)
-		if r.op == "eof" {
-			r.e = edge{x: -1, y: -1}
-			ine <- r
-			inv <- map[int]bool{-1: true}
+		if e.x <= 0 || e.y <= 0 {
+			fmt.Println("Incorrect edge format on input file")
 			break
-		} else {
-			_, err = fmt.Fscanf(file, "%d%d\n", &r.e.x, &r.e.y)
-			ine <- r
-			check(err)
 		}
+		ine <- e
 	}
 	close(ine)
+	inv <- map[int]bool{-1: true}
 	close(inv)
 	file.Close()
 }
@@ -107,31 +98,25 @@ func sink(ostream string, istream string, inv <-chan map[int]bool, endchan chan<
 }
 
 // Generator stage
-func generator(ine <-chan request, inv <-chan map[int]bool, outv chan<- map[int]bool) {
+func generator(ine <-chan edge, inv <-chan map[int]bool, outv chan<- map[int]bool) {
 	for {
-		r, ok := <-ine
+		e, ok := <-ine
 		if ok {
-			switch r.op {
-			case "insert":
-				fmt.Printf("Filter instance created with x = %v, y = %v\n", r.e.x, r.e.y)
-				ineNew := make(chan request)
-				invNew := make(chan map[int]bool)
-				go filter(ine, inv, ineNew, invNew, map[int]bool{r.e.x: true, r.e.y: true})
-				ine = ineNew
-				inv = invNew
-			case "actor2":
-				g, _ := <-inv
-				fmt.Printf("Generator: received actor2 %v\n", g)
-				outv <- g
-			case "eof":
-				g, _ := <-inv
-				fmt.Printf("Generator: received eof %v\n", g)
-				outv <- g
-				break
-			default: //something's wrong
-				fmt.Println("Unknown operation in generator")
-				break
-			}
+			fmt.Printf("Filter instance created with x = %v, y = %v\n", e.x, e.y)
+			ineNew := make(chan edge)
+			invNew := make(chan map[int]bool)
+			go filter(ine, inv, ineNew, invNew, map[int]bool{e.x: true, e.y: true})
+			ine = ineNew
+			inv = invNew
+		} else {
+			break
+		}
+	}
+	for {
+		g, ok := <-inv
+		if _, b := g[-1]; b == false && ok {
+			fmt.Printf("Generator: received eof %v\n", g)
+			outv <- g
 		} else {
 			break
 		}
@@ -140,77 +125,49 @@ func generator(ine <-chan request, inv <-chan map[int]bool, outv chan<- map[int]
 }
 
 // Filter stage
-func filter(ine <-chan request, inv <-chan map[int]bool,
-	oute chan<- request, outv chan<- map[int]bool, cc map[int]bool) {
-	merged := false //Variable used to discard filter instances that have already been united with an actor2
+func filter(ine <-chan edge, inv <-chan map[int]bool,
+	oute chan<- edge, outv chan<- map[int]bool, cc map[int]bool) {
+
+	//Actor1 Phase
 	for {
-		r, ok := <-ine
+		e, ok := <-ine
 		if ok {
-			switch r.op {
-			case "eof":
-				g, _ := <-inv
-				if len(g) == 1 { //g is sent from the source stage so no union must be made
-					fmt.Printf("eof: Passing %v\n", cc)
-					oute <- r
-					outv <- cc
-				} else if merged == false {
-					if intersection(cc, g) == true { //the components are connected so they are merged
-						ccunion := union(cc, g)
-						fmt.Printf("eof: Passing union %v\n", ccunion)
-						oute <- r
-						outv <- ccunion
-					} else { //the components are not connected so they are passed separately
-						r.op = "actor2"
-						fmt.Printf("eof: No intersection, passing actor2 with %v\n", g)
-						oute <- r
-						outv <- g
-						r.op = "eof"
-						fmt.Printf("eof: No intersection, passing eof with %v\n", cc)
-						oute <- r
-						outv <- cc
-					}
-				} else {
-					fmt.Printf("Deactivated stage %v. Passing %v", cc, g)
-					oute <- r
-					outv <- g
-				}
-				break
-			case "actor2":
-				g, _ := <-inv
-				oute <- r
-				if intersection(cc, g) == true && merged == false { //the components are connected so they are merged
-					ccunion := union(cc, g)
-					merged = true
-					fmt.Printf("actor2 with %v: Passing union %v. Deactivated stage\n", cc, ccunion)
-					outv <- ccunion
-				} else { //the components are not connected so they are not merged
-					fmt.Printf("actor2 with %v: No intersection, passing %v\n", cc, g)
-					outv <- g
-				}
-			case "insert":
-				if _, ok := cc[r.e.x]; ok == true { //if cc contains x, then y is added to cc
-					cc[r.e.y] = true
-					//fmt.Printf("Inserted edge with x = %v, y = %v, to %v\n", r.e.x, r.e.y, cc)
-				} else if _, ok := cc[r.e.y]; ok == true { //otherwise, if cc contains y, then x is added to cc
-					cc[r.e.x] = true
-					//fmt.Printf("Inserted edge with x = %v, y = %v, to %v\n", r.e.x, r.e.y, cc)
-				} else { //otherwise r is passed to the next stage
-					oute <- r
-				}
-			default: //something's wrong
-				fmt.Println("Unknown operation in generator")
-				break
+			if _, ok := cc[e.x]; ok == true { //if cc contains x, then y is added to cc
+				cc[e.y] = true
+			} else if _, ok := cc[e.y]; ok == true { //otherwise, if cc contains y, then x is added to cc
+				cc[e.x] = true
+			} else { //otherwise r is passed to the next stage
+				oute <- e
 			}
 		} else {
 			break
 		}
 	}
 	close(oute)
+
+	//Actor2 Phase
+	for {
+		g, _ := <-inv
+		if _, ok := g[-1]; ok == true { //eof so no more sets of vertices will be received
+			fmt.Printf("Actor2 eof: Passing %v\n", cc)
+			break
+		} else {
+			if intersection(cc, g) == true { //the components are connected so they are merged
+				cc = union(cc, g)
+				fmt.Printf("Actor2 merge: %v\n", cc)
+			} else { //the components are not connected so they are passed separately
+				fmt.Printf("Actor2 %v no intersection: Passing %v\n", cc, g)
+				outv <- g
+			}
+		}
+	}
+	outv <- cc
+	outv <- map[int]bool{-1: true}
 	close(outv)
 }
 
 func main() {
-	ine := make(chan request)       //channel transporting requests
+	ine := make(chan edge)          //channel transporting requests
 	inv := make(chan map[int]bool)  //channel transporting sets of connected vertices
 	outv := make(chan map[int]bool) //channel transporting sets of connected vertices
 	endchan := make(chan string)    //channel transporting sets of connected vertices
